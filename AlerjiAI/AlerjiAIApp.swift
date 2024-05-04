e5422342d96455f87819581b40f06a607d661456
Ibrahim Cicek <97982880+CicekIbrahim@users.noreply.github.com>
2024-05-04 21:29:49 +0300

    Initial Commit

---
+++ AlerjiAI/AlerjiAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlerjiAIApp.swift
+//  AlerjiAI
+//
+//  Created by Ibrahim Cicek on 4.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlerjiAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlerjiAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlerjiAI
+//
+//  Created by Ibrahim Cicek on 4.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
